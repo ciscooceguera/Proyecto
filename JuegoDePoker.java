3859faee03b93e15ebc862cf0dcf7bf9fdbbbbf5
José Ramón Suffo Peimbert <suffo.jose@uabc.edu.mx>
2025-05-18 14:43:09 -0700

    Agregadas variables para dinero de jugadores, ciegas y bote de la partida.

--- JuegoDePoker.java
+++ JuegoDePoker.java
@@ -3,12 +3,17 @@ import java.util.ArrayList;
 public abstract class JuegoDePoker {
     protected Mazo mazo;
     protected ArrayList<Mano> manos;
-    protected int numJugadores;
+    protected int numJugadores, ciegaPequeña, ciegaGrande, bote;
+    protected int[] dinero;
     public JuegoDePoker(int numJugadores) {
         this.numJugadores = numJugadores;
         mazo = new Mazo();
         manos = new ArrayList<>();
+        dinero = new int[numJugadores];
         inicializarComponentes();
+        ciegaPequeña = 0;
+        ciegaGrande = 0;
+        bote = 0;
     }
     // inicializa el mazo y llama repartir manos
     public void inicializarComponentes() {
